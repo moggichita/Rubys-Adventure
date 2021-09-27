72a9ad522dc482111f41bd0d70b74968a1c47632
al395603 <al395603@uji.es>
2021-09-27 17:01:50 +0200

    ¿Cómo se expresa el movimiento de Ruby en unidades por segundo?

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -7,8 +7,8 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 10;
+        //QualitySettings.vSyncCount = 0;
+        //Application.targetFrameRate = 10;
     }
 
     // Update is called once per frame
@@ -17,8 +17,8 @@ public class RubyController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
-        position.y = position.y + 0.1f * vertical;
+        position.x = position.x + 0.1f * horizontal * Time.deltaTime;
+        position.y = position.y + 0.1f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
