84f3668ec9282d23ed028689799d8a9b0eb51a2a
al395603 <al395603@uji.es>
2021-09-27 16:52:33 +0200

    Pasos 4, 5, 6, 7 y 8
    ¿Cómo se revisan los cambios al script? / ¿Cómo se muestra el valor horizontal en la consola? / ¿Cómo se ajusta el código para el movimiento? / ¿Cómo puedes testear los cambios que hiciste? / Ejercicio: añade movimiento vertical

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -14,9 +14,10 @@ public class RubyController : MonoBehaviour
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        Debug.Log(horizontal);
+        float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
         position.x = position.x + 0.1f * horizontal;
+        position.y = position.y + 0.1f * vertical;
         transform.position = position;
     }
 }
