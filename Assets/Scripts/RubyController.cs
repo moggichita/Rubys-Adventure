f9e1a4264349d232708f02539545ed71c0a5c655
moggito <90263325+moggichita@users.noreply.github.com>
2021-09-28 22:54:05 +0200

    Paso 12, 13, 14 y 15
    ¿Cómo añadir el Tilemap Collision? /
    ¿Cómo optimizar el Tilemap Collider?

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -18,11 +18,10 @@ public class RubyController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Vector2 position = transform.position;
+        Vector2 position = rigidbody2d.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
     }
-
 }
