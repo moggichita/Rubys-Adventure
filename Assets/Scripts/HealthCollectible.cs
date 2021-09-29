f0382a55ae6b5d6df96b53ce5d581627bb5b194b
al395603 <al395603@uji.es>
2021-09-29 20:50:54 +0200

    ¿Cómo crear un script de colección?

---
+++ Assets/Scripts/HealthCollectible.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCollectible : MonoBehaviour
+{
+  
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Object that entered the trigger : " + other);
+    }
+
+}
