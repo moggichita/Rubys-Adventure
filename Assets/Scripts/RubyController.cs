1b555ac709535da21c2fb0c2beb6f10a3223abe7
al395603 <al395603@uji.es>
2021-09-29 21:45:08 +0200

    ¿Cómo definir una propiedad en RubyController?

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -7,6 +7,7 @@ public class RubyController : MonoBehaviour
     public float speed = 3.0f;
 
     public int maxHealth = 5;
+    public int health { get { return currentHealth; }}
     int currentHealth;
 
     Rigidbody2D rigidbody2d;
