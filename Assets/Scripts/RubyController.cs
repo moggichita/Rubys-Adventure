b19926cc01ed359bf99142e7117484b909b5520c
moggito <90263325+moggichita@users.noreply.github.com>
2021-10-05 15:57:53 +0200

    3. ¿Cómo lanzar el proyectil?

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -11,6 +11,7 @@ public class RubyController : MonoBehaviour
 
     public int health { get { return currentHealth; } }
     int currentHealth;
+    public GameObject projectilePrefab;
 
     public float timeInvincible = 2.0f;
     bool isInvincible;
@@ -78,5 +79,15 @@ public class RubyController : MonoBehaviour
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
 
         Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
     }
 }
