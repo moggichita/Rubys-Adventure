24beb70efcf1292e29debac7eae466c8b710ce1e
al395603 <al395603@uji.es>
2021-10-10 03:06:16 +0200

    ¿Cómo crear un script para tu barra Health?

---
+++ Assets/Scripts/UIHealthBar.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHealthBar : MonoBehaviour
+{
+    public Image mask;
+    float originalSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalSize = mask.rectTransform.rect.width;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mask.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, originalSize * value);
+    }
+}
